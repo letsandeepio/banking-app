5cf5980f4cf8f062f1b78207cb45b514f3ccb9dd
Sandeep <sandeepchopra7@gmail.com>
2023-03-06 13:40:26 -0500

    add search functionality

--- src/components/Accounts/AccountsList.tsx
+++ src/components/Accounts/AccountsList.tsx
@@ -6,12 +6,21 @@ const AccountsList = () => {
   const accounts = useBankStore((state) => state.accounts);
   const { setCurrentlySelectedAccount, setMode } = useAppStore();
 
+  const { searchTerm } = useAppStore();
+
+  /* Filter by name or type */
+  const filteredAccounts = accounts.filter(
+    (account) =>
+      account.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      account.type.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <ul
       role='list'
       className='divide-y divide-gray-200 min-w-[384px] max-w-sm flex-1 overflow-y-auto min-h-0 border-r border-gray-200 px-2 pt-2'
     >
-      {accounts.map((account) => (
+      {filteredAccounts.map((account) => (
         <li
           key={account.id}
           className='relative bg-white py-5 px-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 hover:bg-gray-50 '
@@ -27,7 +36,9 @@ const AccountsList = () => {
                 <p className='truncate text-sm font-medium text-gray-900'>
                   {account.name}
                 </p>
-                <p className='truncate text-sm text-gray-500'>{account.type === "current" ? "Current" : "Saving"}</p>
+                <p className='truncate text-sm text-gray-500'>
+                  {account.type === "current" ? "Current" : "Saving"}
+                </p>
               </a>
             </div>
             <div className='flex-shrink-0 whitespace-nowrap text-sm text-gray-500'>
--- src/zustand/appStore.ts
+++ src/zustand/appStore.ts
@@ -9,6 +9,8 @@ interface AppStore {
   currentlySelectedAccount?: string;
   viewMode: Mode;
   transactionMode?: TransactionMode;
+  searchTerm: string;
+  setSearchTerm:(term: string) => void;  
   setMode: (newMode: Mode) => void;
   setTransactionMode: (newMode: TransactionMode) => void;
   setCurrentlySelectedAccount: (newlySelectedAccount?: string) => void;
@@ -18,11 +20,13 @@ const useAppStore = create<AppStore>()(
   persist(
     (set) => ({
       setMode: (newMode: Mode) => set((state) => ({ viewMode: newMode })),
+      setSearchTerm: (term: string) => set((state) => ({ searchTerm: term })),
       setTransactionMode: (newMode: TransactionMode) =>
         set((state) => ({ transactionMode: newMode })),
       setCurrentlySelectedAccount: (newlySelectedAccount?: string) =>
         set((state) => ({ currentlySelectedAccount: newlySelectedAccount })),
       viewMode: "account",
+      searchTerm: "",
     }),
     { name: "app-store" }
   )
